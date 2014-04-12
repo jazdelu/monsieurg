04b26976bf7e5a3643fdc6187bd426917dd5bdd5
jazdelu <lushizhao@qq.com>
2014-04-12 17:00:11 +0800

    review fix

--- product/views.py
+++ product/views.py
@@ -7,10 +7,7 @@ from django.http import Http404
 def review(request,tid):
 	t = ''
 	try:
-		if not tid:
-			t = Theme.objects.filter(published = False)[0]
-		else:
-			t = Theme.objects.get(id = int(tid))
+		t = Theme.objects.get(id = int(tid))
 	except:
 		raise Http404
 	l = Product_Image.objects.filter(theme=t).filter(position='l')
@@ -21,7 +18,8 @@ def review(request,tid):
 
 def latest(request):
 	try:
-		t = t = Theme.objects.filter(published = False)[0]
+		pt = Theme.objects.get(published = True)
+		t = Theme.objects.filter(published = False).filter(pub_date < pt.pub_date[0])
 	except:
 		raise Http404
 	l = Product_Image.objects.filter(theme=t).filter(position='l')
